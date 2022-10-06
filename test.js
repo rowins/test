e9b92124804a1ef4e6b62314774bb1ee737fe40e
rowins <boyrowin@gmail.com>
2022-10-06 12:23:31 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,220 +1,75 @@
-// @ts-check
-const { join } = require("path");
-const { copySync, removeSync } = require("fs-extra");
-const { readdirSync, lstatSync, readFileSync, existsSync, writeFileSync } = require("fs");
+import commonjsPlugin from "@rollup/plugin-commonjs";
+import jsonPlugin from "@rollup/plugin-json";
+import resolvePlugin from "@rollup/plugin-node-resolve";
+import { build as esbuild } from "esbuild";
+import fsExtra from "fs-extra";
+import { join } from "path";
+import { rollup } from "rollup";
+import { terser as terserPlugin } from "rollup-plugin-terser";
+import { promisify } from "util";
+import webpack from "webpack";
 
-const getOverwritableDirectories = (subDirectories, packageName) => {
-  const additionalOverwritablePaths = {
-    "@aws-sdk/client-sts": ["defaultRoleAssumers.ts", "defaultStsRoleAssumers.ts", "defaultRoleAssumers.spec.ts"],
-  };
-  const unoverwritablePaths = {
-    "@aws-sdk/client-transcribe-streaming": ["README.md"],
-  };
-  const overwritablePaths = [
-    "src", // contains all source files
-    "LICENCE",
-    "README.md",
-  ];
-  return subDirectories.filter((path) => {
-    const isUnoverwritablePaths = unoverwritablePaths[packageName]?.indexOf(path) >= 0;
-    const isProtocolTestFolder = packageName.startsWith("@aws-sdk/aws-") && path === "test";
-    const isOverwritableDirectory = overwritablePaths.indexOf(path) >= 0;
-    const isAdditionalOverwritablePaths = additionalOverwritablePaths[packageName]?.indexOf(path) >= 0;
-    return !isUnoverwritablePaths && (isOverwritableDirectory || isProtocolTestFolder || isAdditionalOverwritablePaths);
+import type { SizeReportContext } from "../index";
+
+const { lstat } = fsExtra;
+
+export interface BundlerSizeReportContext extends SizeReportContext {
+  projectDir: string;
+  entryPoint: string;
+}
+
+export const getWebpackSize = async (context: BundlerSizeReportContext): Promise<number> => {
+  const webpackOutputDir = join(context.projectDir, "webpack-dist");
+  const webpackInstance = webpack({
+    entry: context.entryPoint,
+    output: { path: webpackOutputDir, filename: "index.js" },
   });
-};
-
-/**
- * Copy the keys from newly-generated package.json to
- * existing package.json. For each keys in new package.json
- * we prefer the new key. Whereas for the values, we prefer
- * the values in the existing package.json.
- *
- * This behavior enables us removing dependencies/scripts
- * from codegen, but maintain the newer dependency versions
- * in existing package.json
- */
-const mergeManifest = (fromContent = {}, toContent = {}) => {
-  const merged = {};
-  for (const name of Object.keys(fromContent)) {
-    if (fromContent[name].constructor.name === "Object") {
-      if (name === "devDependencies") {
-        // Use same versions of devDependencies across all workspaces.
-        // After moving to yarn modern, we'll use constraints feature to enforce
-        // consistency in dependency versions https://yarnpkg.com/features/constraints
-        const devDepToVersionHash = {
-          "@tsconfig/recommended": "1.0.1",
-          concurrently: "7.0.0",
-          "downlevel-dts": "0.10.1",
-          rimraf: "3.0.2",
-          typedoc: "0.19.2",
-          typescript: "~4.6.2",
-        };
-        fromContent[name] = Object.keys(fromContent[name])
-          .filter((dep) => Object.keys(devDepToVersionHash).includes(dep))
-          .reduce((acc, dep) => ({ ...acc, [dep]: devDepToVersionHash[dep] }), fromContent[name]);
-      }
-      if (name === "scripts" && !fromContent[name]["build:include:deps"]) {
-        fromContent[name]["build:include:deps"] = "lerna run --scope $npm_package_name --include-dependencies build";
-      }
-
-      merged[name] = mergeManifest(fromContent[name], toContent[name]);
-
-      if (name === "scripts" || name === "devDependencies") {
-        // Allow target package.json(toContent) has its own special script or
-        // dev dependencies that won't be overwritten in codegen
-        merged[name] = { ...toContent[name], ...merged[name] };
-      }
-
-      if (name === "scripts" || name === "dependencies" || name === "devDependencies") {
-        // Sort by keys to make sure the order is stable
-        merged[name] = Object.fromEntries(Object.entries(merged[name]).sort());
-      }
-    } else if (name.indexOf("@aws-sdk/") === 0) {
-      // If it's internal dependency, use current version in the repo if not
-      // present in package.json
-      merged[name] = toContent[name] || "*";
-    } else {
-      // If key (say dependency) is present in both codegen and
-      // package.json, we prefer latter
-      merged[name] = toContent[name] || fromContent[name];
-    }
+  const run = promisify(webpackInstance.run);
+  const webpackStats = await run.call(webpackInstance);
+  if (webpackStats.hasErrors()) {
+    throw new Error(webpackStats.toString());
   }
-  return merged;
+  return (await lstat(join(webpackOutputDir, "index.js"))).size;
 };
 
-const copyToClients = async (sourceDir, destinationDir) => {
-  for (const modelName of readdirSync(sourceDir)) {
-    if (modelName === "source") continue;
-
-    const artifactPath = join(sourceDir, modelName, "typescript-codegen");
-    const packageManifestPath = join(artifactPath, "package.json");
-    if (!existsSync(packageManifestPath)) {
-      console.error(`${modelName} generates empty client, skip.`);
-      continue;
-    }
-
-    const packageManifest = JSON.parse(readFileSync(packageManifestPath).toString());
-    const packageName = packageManifest.name;
-    const clientName = packageName.replace("@aws-sdk/", "");
-
-    console.log(`copying ${packageName} from ${artifactPath} to ${destinationDir}`);
-    const destPath = join(destinationDir, clientName);
-
-    // Code to move files/folders prefixed with `doc-client-` to `lib/lib-dynamodb`
-    if (clientName === "client-dynamodb") {
-      for (const packageSub of readdirSync(artifactPath)) {
-        if (packageSub.startsWith("doc-client-")) {
-          const destinationFileName = packageSub.replace("doc-client-", "");
-          const docClientArtifactPath = join(artifactPath, packageSub);
-          const docClientDestPath = join(destinationDir, "..", "lib", "lib-dynamodb", "src", destinationFileName);
-          copySync(docClientArtifactPath, docClientDestPath, { overwrite: true });
-          removeSync(docClientArtifactPath);
-        }
+export const getRollupSize = async (context: BundlerSizeReportContext): Promise<number> => {
+  const rollupOutputDir = join(context.projectDir, "rollup-dist");
+  const bundle = await rollup({
+    input: context.entryPoint,
+    treeshake: true,
+    plugins: [
+      resolvePlugin({
+        // Unlike webpack, nodejs plugin provides built-in polyfills for "events" in packages like lib-storage
+        preferBuiltins: true,
+      }),
+      commonjsPlugin(),
+      jsonPlugin(),
+      terserPlugin(),
+    ],
+    // Omit circular dependency warning: Circular dependency: node_modules/@aws-crypto/crc32/build/index.js ->
+    // node_modules/@aws-crypto/crc32/build/aws_crc32.js -> node_modules/@aws-crypto/crc32/build/index.js
+    onwarn: (warning, defaultHandler) => {
+      if (warning.code !== "CIRCULAR_DEPENDENCY") {
+        defaultHandler(warning);
       }
-    }
-
-    const packageSubs = readdirSync(artifactPath);
-    const overWritableSubs = getOverwritableDirectories(packageSubs, packageName);
-    for (const packageSub of packageSubs) {
-      const packageSubPath = join(artifactPath, packageSub);
-      const destSubPath = join(destPath, packageSub);
-
-      if (packageSub === "package.json") {
-        //copy manifest file
-        const destManifest = existsSync(destSubPath) ? JSON.parse(readFileSync(destSubPath).toString()) : {};
-        const mergedManifest = {
-          ...mergeManifest(packageManifest, destManifest),
-          homepage: `https://github.com/aws/aws-sdk-js-v3/tree/main/clients/${clientName}`,
-          repository: {
-            type: "git",
-            url: "https://github.com/aws/aws-sdk-js-v3.git",
-            directory: `clients/${clientName}`,
-          },
-        };
-        // no need for the default prepack script
-        delete mergedManifest.scripts.prepack;
-        writeFileSync(destSubPath, JSON.stringify(mergedManifest, null, 2).concat(`\n`));
-      } else if (packageSub === "typedoc.json") {
-        const typedocJson = {
-          extends: "../../typedoc.client.json",
-        };
-        writeFileSync(destSubPath, JSON.stringify(typedocJson, null, 2).concat(`\n`));
-      } else if (overWritableSubs.includes(packageSub) || !existsSync(destSubPath)) {
-        if (lstatSync(packageSubPath).isDirectory()) removeSync(destSubPath);
-        copySync(packageSubPath, destSubPath, {
-          overwrite: true,
-        });
-      }
-    }
-  }
+    },
+  });
+  await bundle.write({
+    file: join(rollupOutputDir, "index.js"),
+  });
+  await bundle.close();
+  return (await lstat(join(rollupOutputDir, "index.js"))).size;
 };
 
-const copyServerTests = async (sourceDir, destinationDir) => {
-  for (const modelName of readdirSync(sourceDir)) {
-    if (modelName === "source") continue;
-
-    const artifactPath = join(sourceDir, modelName, "typescript-ssdk-codegen");
-    const packageManifestPath = join(artifactPath, "package.json");
-    if (!existsSync(packageManifestPath)) {
-      console.error(`${modelName} generates empty server, skip.`);
-      continue;
-    }
-
-    const packageManifest = JSON.parse(readFileSync(packageManifestPath).toString());
-    const packageName = packageManifest.name;
-    const testName = packageName.replace("@aws-sdk/", "");
-
-    console.log(`copying ${packageName} from ${artifactPath} to ${destinationDir}`);
-    const destPath = join(destinationDir, testName);
-
-    const packageSubs = readdirSync(artifactPath);
-    const overWritableSubs = getOverwritableDirectories(packageSubs, packageName);
-    for (const packageSub of packageSubs) {
-      const packageSubPath = join(artifactPath, packageSub);
-      const destSubPath = join(destPath, packageSub);
-
-      if (packageSub === "package.json") {
-        //copy manifest file
-        const destManifest = existsSync(destSubPath) ? JSON.parse(readFileSync(destSubPath).toString()) : {};
-        const mergedManifest = {
-          ...mergeManifest(packageManifest, destManifest),
-          homepage: `https://github.com/aws/aws-sdk-js-v3/tree/main/private/${testName}`,
-          repository: {
-            type: "git",
-            url: "https://github.com/aws/aws-sdk-js-v3.git",
-            directory: `private/${testName}`,
-          },
-        };
-        if (!mergedManifest.scripts.test) {
-          mergedManifest.scripts.test = "jest --coverage --passWithNoTests";
-        }
-        writeFileSync(destSubPath, JSON.stringify(mergedManifest, null, 2).concat(`\n`));
-      } else if (overWritableSubs.includes(packageSub) || !existsSync(destSubPath)) {
-        if (lstatSync(packageSubPath).isDirectory()) removeSync(destSubPath);
-        copySync(packageSubPath, destSubPath, {
-          overwrite: true,
-        });
-      }
-      const jestConfigPath = join(destPath, "jest.config.js");
-      writeFileSync(
-        jestConfigPath,
-        'const base = require("../../jest.config.base.js");\n' +
-          "\n" +
-          "module.exports = {\n" +
-          "  ...base,\n" +
-          "  globals: {\n" +
-          "    'ts-jest': {\n" +
-          "      isolatedModules: true\n" +
-          "    }\n" +
-          "  }\n" +
-          "};\n"
-      );
-    }
-  }
-};
-
-module.exports = {
-  copyToClients,
-  copyServerTests,
+export const getEsbuildSize = async (context: BundlerSizeReportContext): Promise<number> => {
+  const esbuildOutputDir = join(context.projectDir, "esbuild-dist");
+  await esbuild({
+    entryPoints: [context.entryPoint],
+    outbase: context.projectDir,
+    outdir: esbuildOutputDir,
+    bundle: true,
+    minify: true,
+    treeShaking: true,
+  });
+  return (await lstat(join(esbuildOutputDir, "index.js"))).size;
 };
